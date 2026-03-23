85800df9af54912118b994f60e0c04eb72b259e5
ElSai29 <elias.nocq2024@gmail.com>
2026-03-23 12:02:44 +0100

    Initial commit

---
+++ src/main/java/com/accenture/projectPizzeria/ProjectPizzeriaApplication.java
@@ -0,0 +1,21 @@
+package com.accenture.projectPizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Project pizzeria application.
+ */
+@SpringBootApplication
+public class ProjectPizzeriaApplication {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+		SpringApplication.run(ProjectPizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/projectPizzeria/ProjectPizzeriaApplicationTests.java
@@ -0,0 +1,20 @@
+package com.accenture.projectPizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+/**
+ * The type Project pizzeria application tests.
+ */
+@SpringBootTest
+class ProjectPizzeriaApplicationTests {
+
+	/**
+	 * Context loads.
+	 */
+	@Test
+	void contextLoads() {
+	}
+
+}
